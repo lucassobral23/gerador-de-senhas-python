dd2b783500e46ce63be7c5236a532b0394fd7089
Lucas <SEUEMAILDOGITHUB>
2026-05-09 15:00:49 -0300

    Primeiro projeto Python

---
+++ gerador_senhas.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+print("=== GERADOR DE SENHAS ===")
+
+try:
+    tamanho = int(input("Digite o tamanho da senha: "))
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+
+    senha = ""
+
+    for i in range(tamanho):
+        senha += random.choice(caracteres)
+
+    print("\nSenha gerada:")
+    print(senha)
+
+except:
+    print("Digite apenas números!")
